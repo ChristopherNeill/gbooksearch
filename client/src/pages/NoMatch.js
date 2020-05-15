d32a7fc03f7ae79b326e8430abcdf9374c93f84a
christopher <christopherneill2019@icloud.com>
2020-05-14 21:45:42 -0500

    first commit

---
+++ client/src/pages/NoMatch.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function NoMatch() {
+	return (
+		<div className="container">
+			<div className="jumbotron text-center">
+				<h1>404 Page Not Found</h1>
+				<h1>
+					BOOK NOT FOUND
+				</h1>
+			</div>
+		</div>
+	);
+}
+
+export default NoMatch;
